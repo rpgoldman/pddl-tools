3470ad30f6705aaa683c0f2d2f6083daee470242
Robert P. Goldman <rpgoldman@sift.net>
2023-07-12 19:57:55 -0500

    Revise the test scripts.
    Previously, we used `roswell`, but roswell is bit-rotted.

---
+++ do-test.lisp
@@ -0,0 +1,45 @@
+#|
+ This file is a part of pddl-tools
+ (c) 2023 Robert P. Goldman and SIFT, LLC
+ Author: Robert P. Goldman <rpgoldman@sift.net>
+|#
+
+
+(in-package :common-lisp-user)
+
+(defpackage test
+  (:use :common-lisp))
+
+(in-package :test)
+
+(load (merge-pathnames (make-pathname :directory '(:relative "quicklisp") :name "setup" :type "lisp")
+                       (user-homedir-pathname)))
+
+(push (namestring (uiop:pathname-directory-pathname *load-truename*)) ql:*local-project-directories*)
+
+(handler-case
+ (assert (uiop:pathname-equal (asdf:component-pathname (asdf:find-system "pddl-utils"))
+                              (uiop:pathname-directory-pathname *load-truename*)))
+  (error () (uiop:die 2 "Not loading RANDOM-STATE from the expected location.")))
+
+;;; check for clean build
+(defmacro build-system (name)
+ `(handler-bind
+     ((error #'(lambda (c) (uiop:die 3 "Failed to build ~a cleanly with error:~%~T~a" ,name c))))
+   (let ((asdf:*compile-file-failure-behaviour* :error)
+         (asdf:*compile-file-warnings-behaviour* :error))
+     (ql:quickload ,name :silent nil :verbose t))))
+
+(build-system "pddl-utils")
+;; (build-system "hddl-utils")
+
+(defmacro test-system (name)
+  `(handler-case
+    (asdf:test-system ,name)
+  (asdf:fiveam-asdf-failure ()
+    (uiop:quit 1))))
+
+(test-system "pddl-utils")
+;; (test-system "hddl-utils")
+
+(uiop:quit 0)
