4458953889bf0b7a8b96e03926044664bb747a7d
Robert P. Goldman <rpgoldman@sift.net>
2023-07-13 11:17:26 -0500

    Fix loading dependencies.
    fiveam-asdf must be loaded before the system definitions are read:
    :defsystem-depends-on does NOT work for this.

--- do-test.lisp
+++ do-test.lisp
@@ -26,20 +26,22 @@
 
 ;;; check for clean build
 (defmacro build-system (name)
- `(handler-bind
-     ((error #'(lambda (c) (uiop:die 3 "Failed to build ~a cleanly with error:~%~T~a" ,name c))))
-   (let ((asdf:*compile-file-failure-behaviour* :error)
-         (asdf:*compile-file-warnings-behaviour* :error))
-     (ql:quickload ,name :silent nil :verbose t))))
+  `(handler-bind
+       ((error #'(lambda (c) (uiop:die 3 "Failed to build ~a cleanly with error:~%~T~a" ,name c))))
+     (let ((asdf:*compile-file-failure-behaviour* :error)
+           (asdf:*compile-file-warnings-behaviour* :error))
+       (ql:quickload ,name :silent nil :verbose t))))
+
+(ql:quickload "fiveam-asdf")            ; without this, the ASDF defsystems don't load properly.
 
 (build-system "pddl-utils")
 ;; (build-system "hddl-utils")
 
 (defmacro test-system (name)
   `(handler-case
-    (asdf:test-system ,name)
-  (asdf:fiveam-asdf-failure ()
-    (uiop:quit 1))))
+       (asdf:test-system ,name)
+     (asdf:fiveam-asdf-failure ()
+       (uiop:quit 1))))
 
 (test-system "pddl-utils")
 ;; (test-system "hddl-utils")
